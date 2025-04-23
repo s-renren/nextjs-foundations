8e2f4ea778ed3029950e9bcde58b3db1600ccbe3
s-renren <s1f102301156@iniad.org>
2025-04-23 23:07:36 +0900

    7章

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,19 +1,18 @@
-// import postgres from 'postgres';
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
+import postgres from "postgres";
+import { NextResponse } from "next/server";
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+async function listInvoices() {
+  const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
 
-// 	return data;
-// }
+  return data;
+}
 
 export async function GET() {
   try {
@@ -23,9 +22,8 @@ export async function GET() {
       JOIN customers ON invoices.customer_id = customers.id
       WHERE invoices.amount = 666;
     `;
-
-    return NextResponse.json({ data: result.rows });
+    return new Response('Query executed successfully', { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error: 'Query failed', details: error }, { status: 500 });
+    return new Response('Query failed', { status: 500 });
   }
 }
